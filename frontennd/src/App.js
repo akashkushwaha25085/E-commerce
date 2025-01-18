f93781a96c46a0a77922dbd6454ad0ff7f8b57a6
akashkushwaha25085 <142476870+akashkushwaha25085@users.noreply.github.com>
2025-01-18 20:49:01 +0530

    fist time chnage

---
+++ frontennd/src/App.js
@@ -0,0 +1,38 @@
+import "./App.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import SignUp from "./components/SignUp";
+import PrivateComponent from "./components/PrivateComponent";
+import Login from "./components/Login";
+import AddProduct from "./components/AddProduct";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div className="App">
+
+      <BrowserRouter>
+        <Nav />
+
+        <Routes>
+
+          <Route element={<PrivateComponent />}>
+            <Route path="/" element={ <ProductList />  } /> 
+            <Route path="/add" element={ <AddProduct /> } />
+            <Route path="/update" element={<h1>Update product Component</h1>} />
+            <Route path="/logout" element={<h1>logout product Component</h1>} />
+            <Route path="/profile" element={<h1>profile product Component</h1>} />
+          </Route>
+
+          <Route path="signup" element={<SignUp />} />
+          <Route path="login"  element={ <Login /> } />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontennd/src/components/Login.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+const Login = () => {
+   const [email, setEmail] = React.useState('')
+   const [password, setPassword] = React.useState('')
+   const navigate = useNavigate()
+
+useEffect(()=>{
+   const auth = localStorage.getItem("user")
+   if(auth){
+      navigate("/")
+   }
+})
+
+   const handleLogin = async () => {
+
+      console.warn(email, password)
+      let result = await fetch('http://localhost:5000/login', {
+         method: 'post',
+         body: JSON.stringify({ email, password }),
+         headers: {
+            "Content-Type": "application/json"
+         }
+      })
+      result = await result.json()
+      console.log(result);
+      if (result.name) {
+         localStorage.setItem("user", JSON.stringify(result))
+         navigate("/")
+      } else {
+         alert("please enter correct details")
+      }
+
+   }
+
+   return (
+      <div className='login'>
+         <h1> Login </h1>
+         <input className='inputBox' type='text' placeholder='Enter email'
+            onChange={(e) => setEmail(e.target.value)} value={email} />
+         <input className='inputBox' type='password' placeholder='Enter password'
+            onChange={(e) => setPassword(e.target.value)} value={password} />
+         <button onClick={handleLogin} className="appbutton" type="submit">Login</button>
+      </div>
+   )
+}
+
+export default Login
